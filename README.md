252518a2a6d31e486ac0aee7bdd106bb84b17aea
kkopitehong <kkopite.hong@smartcorelink.com>
2022-05-13 10:23:03 +0800

    feat: change auto copy to manually

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # greasyfork-script
 
 一些油候脚本集合
+
+- [在删除github的弹窗里，添加复制按钮用来复制仓库名](https://greasyfork.org/zh-CN/scripts/444882-auto-copy-github-repository-name)
--- auto-copy-repository-name.js
+++ auto-copy-repository-name.js
@@ -1,19 +1,111 @@
 // ==UserScript==
 // @name         Auto-Copy-Github-Repository-Name
 // @namespace    http://tampermonkey.net/
-// @version      0.0.1
-// @description  copy github repository name when deleteing a repository
+// @version      0.0.2
+// @description  add an btn that can copy github repository name when deleteing a repository
 // @author       kkopite
-// @match        https://github.com/**/*/settings
+// @match        https://github.com/*
+// @require      https://code.jquery.com/jquery-3.6.0.slim.min.js
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=github.com
 // @grant        none
 // ==/UserScript==
 
-(function() {
-  'use strict';
-  const pathname = window.location.pathname
-  const index = pathname.lastIndexOf('/settings')
-  const name = pathname.slice(1, index)
-  navigator.clipboard.writeText(name)
+(function () {
+  'use strict'
 
-})();
+  waitForKeyElements('h2#danger-zone', addBtn)
+  function addBtn() {
+    const form = document.querySelector('[aria-label="Delete repository"] form')
+    const parent = form.parentElement
+    if (parent.querySelector('#copy-repository-name')) return
+    const btn = document.createElement('button')
+    btn.id = 'copy-repository-name'
+    parent.insertBefore(btn, form)
+    btn.innerText = 'Copy'
+    btn.classList.add('btn', 'btn-danger', 'm-1')
+    btn.addEventListener('click', copy)
+  }
+  function copy() {
+    const pathname = window.location.pathname
+    const index = pathname.lastIndexOf('/settings')
+    const name = pathname.slice(1, index)
+    navigator.clipboard.writeText(name)
+  }
+
+  // https://gist.github.com/BrockA/2625891
+  function waitForKeyElements(
+    selectorTxt,    /* Required: The jQuery selector string that
+                      specifies the desired element(s).
+                  */
+    actionFunction, /* Required: The code to run when elements are
+                      found. It is passed a jNode to the matched
+                      element.
+                  */
+    bWaitOnce,      /* Optional: If false, will continue to scan for
+                      new elements even after the first match is
+                      found.
+                  */
+    iframeSelector  /* Optional: If set, identifies the iframe to
+                      search.
+                  */
+  ) {
+    var targetNodes, btargetsFound
+
+    if (typeof iframeSelector == "undefined")
+      targetNodes = $(selectorTxt)
+    else
+      targetNodes = $(iframeSelector).contents()
+        .find(selectorTxt)
+
+    if (targetNodes && targetNodes.length > 0) {
+      btargetsFound = true
+      /*--- Found target node(s).  Go through each and act if they
+          are new.
+      */
+      targetNodes.each(function () {
+        var jThis = $(this)
+        var alreadyFound = jThis.data('alreadyFound') || false
+
+        if (!alreadyFound) {
+          //--- Call the payload function.
+          var cancelFound = actionFunction(jThis)
+          if (cancelFound)
+            btargetsFound = false
+          else
+            jThis.data('alreadyFound', true)
+        }
+      })
+    }
+    else {
+      btargetsFound = false
+    }
+
+    //--- Get the timer-control variable for this selector.
+    var controlObj = waitForKeyElements.controlObj || {}
+    var controlKey = selectorTxt.replace(/[^\w]/g, "_")
+    var timeControl = controlObj[controlKey]
+
+    //--- Now set or clear the timer as appropriate.
+    if (btargetsFound && bWaitOnce && timeControl) {
+      //--- The only condition where we need to clear the timer.
+      clearInterval(timeControl)
+      delete controlObj[controlKey]
+    }
+    else {
+      //--- Set a timer, if needed.
+      if (!timeControl) {
+        timeControl = setInterval(function () {
+          waitForKeyElements(selectorTxt,
+            actionFunction,
+            bWaitOnce,
+            iframeSelector
+          )
+        },
+          300
+        )
+        controlObj[controlKey] = timeControl
+      }
+    }
+    waitForKeyElements.controlObj = controlObj
+  }
+})()
