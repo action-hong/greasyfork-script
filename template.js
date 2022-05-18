bcc94218ffde8679e96446df4529165b9033ffe0
kkopitehong <kkopite.hong@smartcorelink.com>
2022-05-18 15:35:42 +0800

    chore: add template

---
+++ template.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name                英文名
+// @name:zh-CN          中文名
+// @namespace           http://tampermonkey.net/
+// @version             0.0.1
+// @description         英文简述
+// @description:zh-CN   中文简述
+// @author              kkopite
+// @match               https://github.com/*
+// @require             https://code.jquery.com/jquery-3.6.0.slim.min.js
+// @icon                https://www.google.com/s2/favicons?sz=64&domain=github.com
+// @grant               none
+// ==/UserScript==
+
+(function () {
+  'use strict'
+
+})()
